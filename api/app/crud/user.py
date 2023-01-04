e7aeb9ea0377ce56f07ef7c9908d94e8bff9225d
CyrilG <gardenat.cyril@cairn-devices.eu>
2023-01-04 23:50:22 +0100

    dd

--- api/app/crud/user.py
+++ api/app/crud/user.py
@@ -1,36 +1,31 @@
 from sqlalchemy.orm import Session
-from  app import models 
-from app.schemas import user as schemas
+
+from app import models
 
 
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+def get_user(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
 
 
-def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
-
-
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
-
-
-def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+def new_user(db: Session, user: str, password: str, is_admin: bool):
+    db_user = models.User(username=user, password=password, admin=is_admin)
     db.add(db_user)
     db.commit()
-    db.refresh(db_user)
     return db_user
 
 
-def get_items(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Item).offset(skip).limit(limit).all()
+def get_all_users(db: Session):
+    return db.query(models.User).all()
 
 
-def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-    db_item = models.Item(**item.dict(), owner_id=user_id)
-    db.add(db_item)
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user is None:
+        return None
+    db.delete(db_user)
     db.commit()
-    db.refresh(db_item)
-    return db_item
+    return db_user
--- api/app/router_utils.py
+++ api/app/router_utils.py
@@ -1,13 +1,16 @@
 import datetime
 import os
 from fastapi import Depends, HTTPException, status
+from jose import JWTError, jwt  # type: ignore
 from sqlalchemy import create_engine
 import sqlalchemy  # type: ignore
 from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy import create_engine
-from app import models
+from app.crud.log import add_log
+from app.schemas.log import LogCreate
 
+from app import auth, models  # type: ignore
 import app.crud.user as crud_user
+
 
 
 POSTGRES_USER = os.getenv('POSTGRES_USER')
@@ -20,12 +23,6 @@ SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{PO
 
 # Dependency
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-    )
-models.Base.metadata.create_all(engine)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
     db = SessionLocal()
@@ -33,3 +30,52 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def logger(db: Session, user, action):
+    log = LogCreate(user_id=user.id, action=action, timestamp=datetime.datetime.now())
+    add_log(db, log)
+
+
+def error_to_status_code(error: Exception):
+    """
+    Convert an exception to an HTTP status code.
+    """
+    if type(
+        error
+    ) is sqlalchemy.exc.IntegrityError and "a foreign key constraint fails" in str(
+        error
+    ):
+        return HTTPException(status_code=412, detail="Foreign key constraint failed")
+    elif type(error) is sqlalchemy.exc.IntegrityError and "Duplicate entry" in str(
+        error
+    ):
+        return HTTPException(status_code=409, detail="Duplicate entry")
+    else:
+        print(str(error))
+        return HTTPException(status_code=500, detail="Unknown internal server error")
+
+
+async def get_current_user(
+    token: str = Depends(auth.oauth2_scheme), db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, auth.SECRET_KEY, algorithms=[auth.ALGORITHM])  # type: ignore
+        username: str = payload.get("sub")  # type: ignore
+        if username is None:
+            raise credentials_exception
+        token_data = auth.TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    if token_data is None or token_data.username is None:
+        raise credentials_exception
+    user = crud_user.get_user(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
