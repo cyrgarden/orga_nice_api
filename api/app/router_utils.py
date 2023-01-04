85114954d532896bdd5d54af12811751463c0edd
CyrilG <gardenat.cyril@cairn-devices.eu>
2023-01-04 23:03:56 +0100

    wrong import

--- api/app/router_utils.py
+++ api/app/router_utils.py
@@ -1,7 +1,6 @@
 import datetime
 import os
 from fastapi import Depends, HTTPException, status
-from jose import JWTError, jwt  # type: ignore
 from sqlalchemy import create_engine
 import sqlalchemy  # type: ignore
 from sqlalchemy.orm import sessionmaker, Session
