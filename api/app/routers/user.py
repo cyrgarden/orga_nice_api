a9697fb178d1573cd91cdeb39e40a14f9c2b7348
CyrilG <gardenat.cyril@cairn-devices.eu>
2023-01-04 23:06:07 +0100

    wrong import

--- api/app/routers/user.py
+++ api/app/routers/user.py
@@ -7,8 +7,6 @@ import app.models as models
 import app.schemas.user as schemas
 from app.router_utils import get_db
 
-from .database import SessionLocal, engine
-
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
