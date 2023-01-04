91ce17dc62450d7a1d39ff74d2e197a7be91ac5e
CyrilG <gardenat.cyril@cairn-devices.eu>
2023-01-04 23:20:51 +0100

    adding sessionmaker

--- api/app/router_utils.py
+++ api/app/router_utils.py
@@ -4,11 +4,17 @@ from fastapi import Depends, HTTPException, status
 from sqlalchemy import create_engine
 import sqlalchemy  # type: ignore
 from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import create_engine
 
 import app.crud.user as crud_user
 
 
 # Dependency
+
+engine = create_engine(
+    DB_CONNECTION_STRING,
+)
+models.Base.metadata.create_all(engine)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
