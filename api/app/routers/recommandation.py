d9094455ffa6e7cc97d672ab9206cb29c1ec372e
CyrilG <gardenat.cyril@cairn-devices.eu>
2023-01-14 23:32:25 +0100

    wrong imports in wrong files

--- api/app/routers/recommandation.py
+++ api/app/routers/recommandation.py
@@ -1,7 +1,16 @@
 from sqlalchemy.orm import Session
 from app import models
 from app.schemas.recommandation import Recommandation, RecommandationCreate, RecommandationOrderBy
-import app.crud.recommandation as crud_reco 
+import app.crud.recommandation as crud_reco
+from typing import List
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException, status
+import app.crud.room as crud_room
+import app.models as models
+from app.schemas.room import Room, RoomCreate
+from app.schemas.user import User, UserCreate
+from app.router_utils import get_db, get_current_user, logger, error_to_status_code
+
 
 router: APIRouter = APIRouter(
     prefix="/recommandation",
--- api/app/schemas/recommandation.py
+++ api/app/schemas/recommandation.py
@@ -1,13 +1,5 @@
 from pydantic import BaseModel
 from app.schemas.utils import OrderBy, Search
-from typing import List
-from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, HTTPException, status
-import app.crud.room as crud_room
-import app.models as models
-from app.schemas.room import Room, RoomCreate
-from app.schemas.user import User, UserCreate
-from app.router_utils import get_db, get_current_user, logger, error_to_status_code
 
 
 # Schema
