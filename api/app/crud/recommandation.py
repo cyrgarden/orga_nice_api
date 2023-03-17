193bab96149f7e4a6298c37f9d1aaaf732183ce0
cyrgarden <cyrgarden42@gmail.com>
2023-03-17 18:15:13 +0100

    typo in parameters

--- api/app/crud/recommandation.py
+++ api/app/crud/recommandation.py
@@ -21,7 +21,7 @@ def get_recommandations_filtered(db:Session, type,price, origin_city, maximum_di
         all_reco = db.query(models.Recommandation).filter(models.Recommandation.price <= price).all()
 
     else:
-        all_reco = db.query(models.Recommandation).filter(models.Recommandation.recommandation_type == type).filter(models.Recommandation.price <= price).filter(indispo not in models.recommandation.indispo).all()
+        all_reco = db.query(models.Recommandation).filter(models.Recommandation.recommandation_type == type).filter(models.Recommandation.price <= price).filter(indispo not in models.Recommandation.indispo).all()
     
     origin = get_coordinates(origin_city, 'FR')
     print(origin)
