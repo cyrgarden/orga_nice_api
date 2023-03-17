df022557b69927b0dc959c3073d30e55016a5feb
cyrgarden <cyrgarden42@gmail.com>
2023-03-17 19:08:54 +0100

    bad naming

--- api/app/crud/reco_indispo.py
+++ api/app/crud/reco_indispo.py
@@ -4,8 +4,8 @@ from app.crud.utils import get_all
 from app.schemas.reco_indispo import RecoIndispo, RecoIndispoCreate
 
 
-def create_indispo(db: Session, new_indispo: reco_indispoCreate):
-    db_indispo = models.reco_indispo(**new_indispo.dict())
+def create_indispo(db: Session, new_indispo: RecoIndispoCreate):
+    db_indispo = models.RecoIndispo(**new_indispo.dict())
     db.add(db_indispo)
     db.commit()
     db.refresh(db_indispo)
@@ -13,7 +13,7 @@ def create_indispo(db: Session, new_indispo: reco_indispoCreate):
 
 
 def delete_indispo(db: Session, indispo_id: int):
-    db_indispo = db.query(models.reco_indispo).filter(models.reco_indispo.id == indispo_id).first()
+    db_indispo = db.query(models.RecoIndispo).filter(models.RecoIndispo.id == indispo_id).first()
     if db_indispo is None:
         return None
     db.delete(db_indispo)
